9c29d8bb9f039168d12cc2300a0025894171aa02
johncban <johncban@gmail.com>
2019-05-14 06:50:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,12 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
 
-  def initialize(shoe)
-    @shoe = shoe
+  def initialize(brand)
+    @brand = brand
   end
 
-  def shoe
-    @shoe
+  def brand
+    @brand
   end
 
   def cobble
