9d782add86c6f25a41716c971cc4a86832188d63
johncban <johncban@gmail.com>
2019-05-14 06:31:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,16 @@
 class Book
 
-  def initialize(title)
+  def initialize(title) # getter
     @title = title
   end
 
   def title
     @title
   end
+
+  def author=(author) # setter
+    @author = author
+  end
+
 
 end
